cf4bd4c1647db112e31d6b1ab1ddb924e4b56295
FiorellaCrocco <fiorellacrocco@gmail.com>
2021-08-24 21:13:07 +0000

    Terminando..

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,17 +1,14 @@
-// Your code here:
-const plural = "bottles of milk on the wall,";
-const plural2 = "bottles of milk.";
-let cancion = [];
-
-function sing(){
-    for(let i = 99; i > 1; i--){
-        if (i > 1){
-            cancion.push(plural);
-        } else { 
-            cancion.push(plural);
-        }
+let cancion = "";
+for(let i = 99; i >= 0; i--){
+    // console.log(i);
+    if (i > 1){
+        cancion+=`${i} bottles of milk on the wall, ${i} bottles of milk. Take one down and pass it around, ${i - 1} bottles of milk on the wall.`
+        console.log(cancion);
+    } else if (i==1){
+        cancion+=`${i} bottle of milk on the wall, ${i} bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.`
+        console.log(cancion);
+    } else {
+        cancion+=`No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.`
+        console.log(cancion)
     }
-
-    return cancion.join([i])
 }
-
