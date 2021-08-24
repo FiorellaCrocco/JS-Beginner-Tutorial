e6202b51da6a2da74f7603786fdf4dcbddfba429
FiorellaCrocco <fiorellacrocco@gmail.com>
2021-08-24 04:15:22 +0000

    ...

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,1 +1,17 @@
 // Your code here:
+const plural = "bottles of milk on the wall,";
+const plural2 = "bottles of milk.";
+let cancion = [];
+
+function sing(){
+    for(let i = 99; i > 1; i--){
+        if (i > 1){
+            cancion.push(plural);
+        } else { 
+            cancion.push(plural);
+        }
+    }
+
+    return cancion.join([i])
+}
+
